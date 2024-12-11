37c6212f4fc1f457b61da34cc7bac7941c7a164c
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-12-11 10:48:06 +0900

    12.10(화) - 템플릿 문법_기본 - 템플릿 문법_실전 - 프로젝트 생성 도구_Vue CLI - 싱글 파일 컴포넌트
    12.11(수)
    -  최종프로젝트_사용자 입력 폼 만들기
    - 마무리

---
+++ vue-form/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <form v-on:submit.prevent="submitForm"> 
+    <div>
+      <label for="username"> id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password"> pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit" >login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: () => ({
+    username: '',
+    password: '',
+
+  }),
+  methods: {
+    submitForm: function() {
+      // event.preventDefault(); //코드의 새로고침을 막아줌. --> 기본적인 js에서 사용 
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+      .then(function(respose){
+        console.log(respose);
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
