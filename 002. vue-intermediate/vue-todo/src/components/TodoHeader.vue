15c938fd42ade73c000b3929f6ca7743ef80f97a
eunhyeon <eunhyeon1734@gmail.com>
2025-01-16 10:25:49 +0900

    002. vue-intermediate 폴더 생성

---
+++ 002. vue-intermediate/vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+      TODO it !
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  /* scoped :: 해당 컴포넌트 안에서만 적용 */
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
