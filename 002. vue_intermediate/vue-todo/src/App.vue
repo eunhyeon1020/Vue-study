b7590878e3b4f06de89f1134896f1c4f1d3ffd73
eunhyeon <eunhyeon1734@gmail.com>
2025-04-13 16:27:42 +0900

    vue.js 중급 강좌 (4.12) - 9강 TodoInput 컴포넌트 코드 정리 및 UI 스타일링

--- 002. vue_intermediate/vue-todo/src/App.vue
+++ 002. vue_intermediate/vue-todo/src/App.vue
@@ -13,12 +13,13 @@ import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
+
 export default {
   components : {
     'TodoHeader' : TodoHeader,
     'TodoInput' : TodoInput,
     'TodoList' : TodoList,
-    'TodoFooter' : TodoFooter,
+    'TodoFooter' : TodoFooter
   }
 }
 </script>
@@ -37,8 +38,8 @@ export default {
   button {
     border-style: groove;
   }
-
+  
   .shadow {
-    box-shadow: 5px 10px 10px rgb(0, 0, 0, 0.03);
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
   }
 </style>
--- 002. vue_intermediate/vue-todo/src/components/TodoHeader.vue
+++ 002. vue_intermediate/vue-todo/src/components/TodoHeader.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <h1>
-      TODO it !
-    </h1>
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
@@ -13,9 +11,8 @@ export default {
 </script>
 
 <style scoped>
-  /* scoped :: 해당 컴포넌트 안에서만 적용 */
   h1 {
-    color: #2F3B52;
+    color: #2f3852;
     font-weight: 900;
     margin: 2.5rem 0 1.5rem;
   }
